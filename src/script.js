f4859d129d4d66d6f866d4e25770a6a901d30356
vorobjovsh <vorobjov.sh@mail.ru>
2018-03-24 01:17:55 +0200

    change script.js

--- src/script.js
+++ src/script.js
@@ -2,11 +2,7 @@
 
 //= js/setActiveLink.js
 //= js/search-city.js
-<<<<<<< HEAD
-//= ../node_modules/handlebars/dist/handlebars.js
-=======
 //= js/weathers.js
 
 //= js/templates/headerTemplate.js
 //= js/templates/todayTemplate.js
->>>>>>> other
