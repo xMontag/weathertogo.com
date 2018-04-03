60e95a732669b736a79af249fb0805520e6551fe
vorobjovsh <vorobjov.sh@mail.ru>
2018-04-03 20:33:52 +0300

    added addFavoriteSity.js

--- src/js/search-city.js
+++ src/js/search-city.js
@@ -2,6 +2,7 @@ const inputCity = document.getElementById("search-city");
 const autocomplete = new google.maps.places.Autocomplete(inputCity, {
     types: ["(cities)"]
 });
+const currentPlace = document.getElementById("header-visual__city");
 
 // координаты Киева
 const positions = {
@@ -16,10 +17,10 @@ google.maps.event.addListener(autocomplete, "place_changed", function () {
     // -------------------- отладочный блок -------------------
     console.log(place);
 
-    const currentPlace = document.getElementById("header-visual__city");
     currentPlace.textContent = place.name;
 
     positions.latPositions = place.geometry.location.lat();
     positions.lngPositions = place.geometry.location.lng();
+   
     getWeather(positions.latPositions, positions.lngPositions, 'ru');
 })
