bdcee5ac79703d03333ca973d90fc796ad71df59
vorobjovsh <vorobjov.sh@mail.ru>
2018-04-11 12:01:14 +0300

    change search-city.js

--- src/js/search-city.js
+++ src/js/search-city.js
@@ -26,7 +26,7 @@ google.maps.event.addListener(autocomplete, "place_changed", function () {
     positions.lngPositions = place.geometry.location.lng();
     positions.sityFullName = place.formatted_address;
 
-    if(inputTime === undefined) {
+    if(inputTime === undefined || weatherWay.start !== undefined) {
           weatherWay.start.lat = place.geometry.location.lat();
           weatherWay.start.lng = place.geometry.location.lng();
           weatherWay.start.sityName = place.name;
